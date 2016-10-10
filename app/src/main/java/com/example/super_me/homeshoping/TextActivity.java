3f9b1536c29007d800504c216ae44ad5afad9e2f
Cao <934235403@qq.com>
2016-10-10 20:00:18 +0800

    init

---
+++ app/src/main/java/com/example/super_me/homeshoping/TextActivity.java
@@ -0,0 +1,16 @@
+package com.example.super_me.homeshoping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Super-me on 2016/10/10.
+ */
+
+public class TextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
